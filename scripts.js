cbf360964310032f2b038bcc803717034ac754b1
Filipe-Mamed <filipemamed9@gmail.com>
2024-06-19 20:26:16 -0300

    Atualizando valores em tempo real

--- scripts.js
+++ scripts.js
@@ -1,15 +1,21 @@
 const button = document.querySelector("button");
 const select_moeda = document.querySelector("#select-moeda");
 
-const dolar = 5.2;
-const euro = 5.9;
-const bitcoin = 0.0000027
-
-const converte_moedas = function () {
+const converte_moedas = async function () {
   const inputReais = document.querySelector("input").value;
   const realValor = document.querySelector("#real-valor");
   const valor_moeda = document.querySelector("#valor-moeda");
   realValor.innerHTML = inputReais;
+
+  const data = await fetch(
+    "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL"
+  ).then(function (response) {
+    return response.json();
+  });
+
+  const dolar = data.USDBRL.high;
+  const euro = data.EURBRL.high;
+  const bitcoin = data.BTCBRL.high;
 
   realValor.innerHTML = new Intl.NumberFormat("pt-BR", {
     style: "currency",
@@ -21,16 +27,15 @@ const converte_moedas = function () {
       style: "currency",
       currency: "USD",
     }).format(inputReais / dolar);
-  } else if(select_moeda.value === "€ Euro") {
+  } else if (select_moeda.value === "€ Euro") {
     valor_moeda.innerHTML = new Intl.NumberFormat("de-DE", {
-    style: "currency",
-    currency: "EUR",
+      style: "currency",
+      currency: "EUR",
     }).format(inputReais / euro);
+  } else {
+    select_moeda.value === "₿ Bitcoin";
+    valor_moeda.innerHTML = "₿ " + (inputReais / bitcoin).toFixed(7);
   }
-  else{
-    select_moeda === "₿ Bitcoin"
-    valor_moeda.innerHTML = "₿ " + (inputReais * bitcoin).toFixed(8);
-    }
 };
 
 const change_moedas = function () {
@@ -40,20 +45,17 @@ const change_moedas = function () {
   if (select_moeda.value === "US$ Dólar americano") {
     paragrafo_dinheiro.innerHTML = "Dólar americano";
     img_moeda.src = "./assests/eua.png";
-  } else {
+  } else if (select_moeda.value === "€ Euro") {
     select_moeda.value === "€ Euro";
     paragrafo_dinheiro.innerHTML = "Euro";
     img_moeda.src = "./assests/euro.png";
+  } else if (select_moeda.value === "₿ Bitcoin") {
+    paragrafo_dinheiro.innerHTML = "Bitcoin";
+    img_moeda.src = "./assests/bitcoin.png";
   }
 
-  if(select_moeda.value === "₿ Bitcoin"){
-    paragrafo_dinheiro.innerHTML = "Bitcoin"
-    img_moeda.src = "./assests/bitcoin.png"
-  }
-
-  converte_moedas()
+  converte_moedas();
 };
 
-
-button.addEventListener("click", converte_moedas)
+button.addEventListener("click", converte_moedas);
 select_moeda.addEventListener("change", change_moedas);
